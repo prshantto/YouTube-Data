329747c60c26b75d046a48f88dee5095aa7564a8
Prashant <prashanttt001@gmail.com>
2025-01-14 08:54:39 +0530

    added aiResponse route

--- app.js
+++ app.js
@@ -4,15 +4,38 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const { getJson } = require("serpapi");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World! Server is running...");
 });
 
+app.get("/ai-response", async (req, res) => {
+  const prompt = req.body.prompt;
+  const aiResponse = await axios.post(
+    `${process.env.GEMINI_API_URL}?key=${process.env.GEMINI_API_KEY}`,
+    {
+      contents: [
+        {
+          parts: [
+            {
+              text: prompt,
+            },
+          ],
+        },
+      ],
+    }
+  );
+
+  res.end(JSON.stringify(aiResponse.data));
+});
+
 app.get("/get-yt-video", async (req, res) => {
-  const url = req.query.url;
+  const url = req.body.url;
   function extractVideoId(url) {
     const pattern =
       /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|\S*?[?&]v=|embed\/|v\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
