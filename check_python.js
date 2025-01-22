b5b14dd09f5e3c836b9cd7ebba8b83716a4fb65f
prashant <prashanttt001@gmail.com>
2025-01-22 10:55:38 +0530

    error fix

--- check_python.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require("child_process");
-
-exec("python --version", (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`Python Version: ${stdout}`);
-});
